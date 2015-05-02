992715384104a5bd3b511256168f9a0add2095d1
Ian Williams <me@ianwillia.ms>
2015-05-02 11:23:10 -0400

    Migrating to one gulp file

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,21 +1,45 @@
-if(process.env.NODE_ENV === undefined) process.env.NODE_ENV = 'development';
-
 var gulp = require('gulp');
-var exec = require('child_process').exec;
+var gutil = require('gutil');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var livereload = require('gulp-livereload');
 
+var paths = {
+	scripts: ['public/assets/scripts/**/*.js', '!public/assets/scripts/_dist/*'],
+	styles: ['public/assets/styles/**/*.scss', '!public/assets/styles/_dist/*'],
+	php: ['server/site/**/*.php'],
+	content: ['content/**/*'],
+};
 
-gulp.task('frontend', function() {
-	var devString = process.env.NODE_ENV == 'development' ? 'develop' : '';
-	exec('cd public && gulp ' + devString, function(err, stdout, stderr) {
-		if(err) {
-			console.log('[gulp] [gulp] ', stderr, err);
-		} else {
-			console.log('[gulp] [gulp] ', stdout);
-		}
-	});
+gulp.task('scripts', function() {
+	return browserify('./public/assets/scripts/index.js')
+	.bundle()
+	.pipe(source('index.js'))
+	.pipe(gulp.dest('public/assets/scripts/_dist'))
+	.pipe(livereload());
 });
 
-// === START TASKS
+gulp.task('stylesheets', function() {
+	return gulp.src('./public/assets/styles/index.scss')
+	.pipe(sass({errLogToConsole: true}))
+	.pipe(autoprefixer('last 2 versions'))
+	.pipe(gulp.dest('public/assets/styles/_dist'))
+	.pipe(livereload());
+});
 
-gulp.task('default', ['frontend']);
-gulp.task('develop', ['frontend']);
+gulp.task('reload', function() {
+	livereload.reload();
+});
+
+gulp.task('watch', function() {
+	gulp.watch(paths.scripts, ['scripts']);
+	gulp.watch(paths.styles, ['stylesheets']);
+	gulp.watch(paths.php, ['reload']);
+	gulp.watch(paths.content, ['reload']);
+	livereload.listen();
+});
+
+gulp.task('compile', ['scripts', 'stylesheets']);
+gulp.task('default', ['compile', 'watch']);
